3872c28844abe624b71d523affc29bd0c5f38d3a
Bertan Günyel <bertan.gunyel@gmail.com>
2024-09-18 11:49:15 +0300

    Adjustments in Pydantic Config

--- source/config.py
+++ source/config.py
@@ -1,20 +1,31 @@
 import os
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, BaseModel
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ENV_FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, os.pardir))
 
 
+class TinyImageNet(BaseModel):
+    TRAIN_FILE: str
+    VALID_FILE: str  # Validation File
+
+
 class Settings(BaseSettings):
+
     APPLICATION_NAME: str = "Computer Vision Fun"
 
     DATA_FOLDER: str
     OUT_FOLDER: str
 
+    TINY_IMAGENET: TinyImageNet
+
     class Config:
         case_sensitive = True
         env_file_encoding = "utf-8"
         env_file = os.path.join(ENV_FILE_DIR, '.env')
+        env_nested_delimiter = '__'
+        extra='allow'
 
 
 class Constants(BaseSettings):
@@ -28,4 +39,3 @@ class ModelSettings(BaseSettings):
 
 settings = Settings()
 model_settings = ModelSettings()
-
